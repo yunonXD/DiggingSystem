835771121334868dfda9c84489dd0f0131dd46e9
yunon <yunonXD@users.noreply.github.com>
2022-04-26 16:45:38 +0900

    DiggingSystem
    Ver_1

---
+++ Digging/Assets/Dig/Scripts/DigRender.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DigRender : MonoBehaviour
+{
+    public enum BlendType
+    {
+        Add, 
+        Sub
+    }
+
+
+}
---
+++ Digging/Assets/Scripts/ShowSystemInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowSystemInfo : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<UnityEngine.UI.Text>().text = SystemInfo.graphicsDeviceType + "-" + SystemInfo.operatingSystem;
+    }
+}
